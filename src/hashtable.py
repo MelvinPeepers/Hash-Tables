0f37953ece5a91aae96784092b1270cb547e5572
Melvin <peepersm@gmail.com>
2020-04-11 23:07:23 -0400

    collision

--- src/hashtable.py
+++ src/hashtable.py
@@ -22,6 +22,7 @@ class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
+        self.size = 0  # number of elements that have been inserted
 
     def _hash(self, key):
         '''
@@ -62,22 +63,37 @@ class HashTable:
 
         This
         '''
+        self.size += 1  # increment size
         # Hashmod the key to find the bucket
         index = self._hash_mod(key)  # Compute index of key
         # Check if a pair already exisit in the bucket
         pair = self.storage[index]  # Go to the node corresponding to the hash
-        if pair is not None:
-            # If bucket is empty, overwrite the key/value and throw a warning
-            if pair.key != key:
-                print("Warning: Overwriting value")
-                pair.key = key
-            pair.value = value
-        else:
-            # If bucket is not empty, create a LinkedPair and place it in the bucket
-            self.storage[index] = LinkedPair(key, value)
 
-        #array_index = self._hash_mod(self._hash(key))
-        #self.storage[array_index] = value
+        # if bucket is empty
+        if pair is None:
+            # create pair, add it return
+            self.storage[index] = LinkedPair(key, value)
+            return
+            # collision iterate to the end of the linked list at provided index
+        prev = pair
+        while pair is not None:
+            prev = pair
+            pair = pair.next
+
+        # Add a new pair at the end of the list with provided key/value
+        prev.next = LinkedPair(key, value)
+
+        #    # If bucket is empty, overwrite the key/value and throw a warning
+        #    if pair.key != key:
+        #        print("Warning: Overwriting value")
+        #        pair.key = key
+        #    pair.value = value
+        # else:
+        #    # If bucket is not empty, create a LinkedPair and place it in the bucket
+        #    self.storage[index] = LinkedPair(key, value)
+
+        # array_index = self._hash_mod(self._hash(key))
+        # self.storage[array_index] = value
 
     def remove(self, key):
         '''
@@ -89,6 +105,27 @@ class HashTable:
 
         THIS
         '''
+        # index = self._hash_mod(key)  # Compute index of key
+        # pair = self.storage[index]
+        # prev = None
+
+        # while pair is not None and pair.key != key:
+        #    prev = pair
+        #    pair = pair.next
+
+        # if pair is None:
+        #    return None
+
+        # else:
+        #    self.size -= 1
+        #    result = pair.value
+        #    if prev is None:
+        #        pair = None
+        #    else:
+        #        prev.next = prev.next.next
+
+        #    return result
+
         index = self._hash_mod(key)  # Compute index of key
         # Check if a pair exists in the bucket with matching keys
         if self.storage[index] is not None and self.storage[index].key == key:
@@ -109,15 +146,30 @@ class HashTable:
         '''
         # Get the index from hashmod
         index = self._hash_mod(key)
-        # Check if a pair exists in the bucket with matching keys
-        if self.storage[index] is not None and self.storage[index].key == key:
-            # If so, return the value
-            return self.storage[index].value
-        else:
-            # Else return None
+
+        pair = self.storage[index]
+
+        while pair is not None and pair.key != key:
+            pair = pair.next
+
+        if pair is None:
+            # Not found
             return None
 
-        #array_index = self._hash_mod(self._hash(key))
+        else:
+            return pair.value  # found: return the data value
+
+        # Get the index from hashmod
+        # index = self._hash_mod(key)
+        # Check if a pair exists in the bucket with matching keys
+        # if self.storage[index] is not None and self.storage[index].key == key:
+        #    # If so, return the value
+        #    return self.storage[index].value
+        # else:
+        #    # Else return None
+        #    return None
+
+        # array_index = self._hash_mod(self._hash(key))
         # return self.storage[array_index]
 
     def resize(self):
@@ -128,7 +180,16 @@ class HashTable:
         Fill this in.
         AND THIS
         '''
-        pass
+        old_storage = self.storage
+        self.capacity *= 2
+
+        # create a new array size * 2
+        self.storage = [None] * self.capacity
+
+        # move all values over
+        for pair in old_storage:
+            # re-insert each key/value
+            self.insert(pair.key, pair.value)
 
 
 if __name__ == "__main__":
